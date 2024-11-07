406bd848a8cda2b9da4d75aa1fa93e8e27dcfaef
apozjebus <mategli6@gmail.com>
2024-11-07 21:32:16 +0100

    wallahi im finished

--- labirynt.cpp
+++ labirynt.cpp
@@ -1,374 +1,315 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
-#include <cstring>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <cstring>
 
 using namespace std;
 
 class File
 {
-	private:
-		string fileName = "labirynth.txt";
-	protected:
-		File()
+private:
+	string fileName = "labirynth.txt";
+
+protected:
+	File()
+	{
+	}
+	File(string fileName)
+	{
+		this->fileName = fileName;
+	}
+	~File()
+	{
+	}
+	string getFileName()
+	{
+		return fileName;
+	}
+	int *ReadSizeFromFile(string fileName)
+	{
+		ifstream file;
+		file.open(fileName.c_str());
+		if (file.is_open())
 		{
-		}
-		File(string fileName)
-		{
-			this->fileName = fileName;
-		}
-		~File()
-		{
-		}
-		string getFileName()
-		{
-			return fileName;
-		}
-		int* ReadSizeFromFile(string fileName)
-		{
-			ifstream file;
-			file.open(fileName.c_str());
-			if (file.is_open())
+			string line;
+			int horizontalSize = 0;
+			int verticalSize = 0;
+			while (getline(file, line))
 			{
-				string line;
-				int horizontalSize = 0;
-				int verticalSize = 0;
-				while (getline(file, line))
+				verticalSize++;
+				if (horizontalSize < line.length())
 				{
-					verticalSize++;
-					if (horizontalSize < line.length())
-					{
-						horizontalSize = line.length();
-					}
+					horizontalSize = line.length();
 				}
-				file.close();
-				int* size = new int[2];
-				size[0] = verticalSize;
-				size[1] = horizontalSize;
-				return size;
 			}
-			else
-			{
-				cout << "File not found" << endl;
-				return NULL;
-			}
+			file.close();
+			int *size = new int[2];
+			size[0] = verticalSize;
+			size[1] = horizontalSize;
+			return size;
 		}
-		char** ReadLabirynthFromFile(string fileName, int horizontalSize, int verticalSize)
+		else
 		{
-			ifstream file;
-			file.open(fileName.c_str());
-			if (file.is_open())
-			{
-				char** labirynth = new char* [verticalSize];
-				int iterator = 0;
-				string line;
-				while (getline(file, line))
-				{
-					char* lineOfLab = new char[line.length() + 1];
-					strcpy_s(lineOfLab, line.length() + 1, line.c_str());
-					labirynth[iterator] = lineOfLab;
-					iterator++;
-				}
-				file.close();
-				return labirynth;
-			}
-			else
-			{
-				cout << "File not found" << endl;
-				return NULL;
-			}
+			cout << "File not found" << endl;
+			return NULL;
 		}
-		char** writeLabirynthToFile(char** labirynth, int horizontalSize, int verticalSize)
+	}
+	char **ReadLabirynthFromFile(string fileName, int horizontalSize, int verticalSize)
+	{
+		ifstream file;
+		file.open(fileName.c_str());
+		if (file.is_open())
 		{
-			ofstream file;
-			fileName = "labirynthCopy.txt";
-			file.open(fileName.c_str(), ios::trunc);
-			if (file.is_open())
+			char **labirynth = new char *[verticalSize];
+			int iterator = 0;
+			string line;
+			while (getline(file, line))
 			{
-				for (int i = 0; i < verticalSize; i++)
-				{
-					for (int j = 0; j < horizontalSize; j++)
-					{
-						file << labirynth[i][j];
-					}
-					file << endl;
-				}
-				file.close();
-				return labirynth;
+				char *lineOfLab = new char[line.length() + 1];
+				strcpy_s(lineOfLab, line.length() + 1, line.c_str());
+				labirynth[iterator] = lineOfLab;
+				iterator++;
 			}
-			else
-			{
-				cout << "File not found" << endl;
-				return NULL;
-			}
+			file.close();
+			return labirynth;
 		}
+		else
+		{
+			cout << "File not found" << endl;
+			return NULL;
+		}
+	}
+	void writeLabirynthToFile(char **labirynth, int horizontalSize, int verticalSize)
+	{
+		ofstream file;
+		file.open("labirynthCopy.txt", ios::trunc);
+		if (file.is_open())
+		{
+			for (int i = 0; i < verticalSize; i++)
+			{
+				for (int j = 0; j < horizontalSize; j++)
+				{
+					file << labirynth[i][j];
+				}
+				file << endl;
+			}
+			file.close();
+		}
+		else
+		{
+			cout << "File not found" << endl;
+		}
+	}
 };
 
 class Labirynth : File
 {
-	private:
-		int verticalSize, horizontalSize, startX, startY, endX, endY;
-		char **labirynth;
-		vector<int*> path;
+private:
+	const int moveX[4] = {1, 0, -1, 0};
+	const int moveY[4] = {0, 1, 0, -1};
+	int WIDTH;
+	int HEIGHT;
+	vector<vector<char>> labirynth;
+	vector<int *> path;
+	int start[2];
+	int end[2];
 
-		int dx[4] = { 0, 0, -2, 2 };
-		int dy[4] = { -2, 2, 0, 0 };
+	bool isValid(int x, int y)
+	{
+		return x > 0 && x < WIDTH - 1 && y > 0 && y < HEIGHT - 1 && labirynth[y][x] == 'x';
+	}
 
-		// Sprawdza, czy komórka jest w granicach
-		bool isInBounds(int x, int y) {
-			return x > 0 && x < verticalSize - 1 && y > 0 && y < horizontalSize - 1;
-		}
+	void generateMaze(int x, int y)
+	{
+		vector<int> dirs = {0, 1, 2, 3};
+		random_shuffle(dirs.begin(), dirs.end());
 
-		// Funkcja generująca labirynt metodą DFS
-		void generateMaze(int x, int y) {
-			// Mieszamy kolejność kierunków, aby uzyskać losowość
-			int directions[4] = { 0, 1, 2, 3 };
-			shuffle(directions, directions + 4);
-
-			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[directions[i]];
-				int ny = y + dy[directions[i]];
-
-				// Sprawdzenie, czy można przebić się w wybranym kierunku
-				if (isInBounds(nx, ny) && labirynth[nx][ny] == 'x') {
-					labirynth[nx][ny] = ' ';
-					labirynth[x + dx[directions[i]] / 2][y + dy[directions[i]] / 2] = ' ';
-					generateMaze(nx, ny);
-				}
-			}
-		}
-    public:
-        Labirynth(int verticalSize, int horizontalSize, int startX, int startY, int endX, int endY)
-        {
-			this->verticalSize = verticalSize;
-			this->horizontalSize = horizontalSize;
-			this->startX = startX;
-			this->startY = startY;
-			this->endX = endX;
-			this->endY = endY;
-			labirynth = new char*[verticalSize];
-			for (int i = 0; i < verticalSize; i++)
-			{
-				labirynth[i] = new char[horizontalSize];
-				for (int j = 0; j < horizontalSize; j++)
-				{
-					labirynth[i][j] = 'o';
-				}
-			}
-			for (int i = 0; i < verticalSize; i++)
-			{
-				labirynth[0][i] = 'x';
-			}
-			for (int i = 0; i < verticalSize; i++)
-			{
-				labirynth[verticalSize - 1][i] = 'x';
-			}
-			for (int i = 0; i < horizontalSize; i++)
-			{
-				labirynth[i][0] = 'x';
-			}
-			for (int i = 0; i < horizontalSize; i++)
-			{
-				labirynth[i][horizontalSize - 1] = 'x';
-			}
-			labirynth[startX][startY] = 'S';
-			labirynth[endX][endY] = 'E';
-			// tu dopisz swoj algorytm
-		}
-		Labirynth()
+		for (int i = 0; i < 4; i++)
 		{
-			int* size = ReadSizeFromFile(getFileName());
-			if (size != NULL)
+			int nx = x + moveX[dirs[i]] * 2;
+			int ny = y + moveY[dirs[i]] * 2;
+
+			if (isValid(nx, ny))
 			{
-				verticalSize = size[0];
-				horizontalSize = size[1];
-				labirynth = ReadLabirynthFromFile(getFileName(), horizontalSize, verticalSize);
-				for (int i = 0; i < verticalSize; i++)
+				labirynth[ny][nx] = ' ';
+				labirynth[y + moveY[dirs[i]]][x + moveX[dirs[i]]] = ' ';
+
+				generateMaze(nx, ny);
+			}
+		}
+	}
+
+	void randomStartEndPoints()
+	{
+		bool set_start = false;
+		while (!set_start)
+		{
+			int random = rand() % 2;
+			if (random == 0)
+			{
+				start[0] = rand() % (HEIGHT - 1);
+				start[1] = 0;
+				if (labirynth[start[1] + 1][start[0]] != 'x')
 				{
-					for (int j = 0; j < horizontalSize; j++)
-					{
-						if (labirynth[i][j] == 'S')
-						{
-							startX = i;
-							startY = j;
-						}
-						if (labirynth[i][j] == 'E')
-						{
-							endX = i;
-							endY = j;
-						}
-					}
+					labirynth[start[1]][start[0]] = 'S';
+					set_start = true;
 				}
 			}
 			else
 			{
-				cout << "File not found" << endl;
-				exit(1);
+				start[0] = 0;
+				start[1] = rand() % (WIDTH - 1);
+				if (labirynth[start[1]][start[0] + 1] != 'x')
+				{
+					labirynth[start[1]][start[0]] = 'S';
+					set_start = true;
+				}
 			}
 		}
-		~Labirynth()
+
+		bool set_end = false;
+		while (!set_end)
 		{
-		}
-		int getVerticalSize()
-		{
-			return verticalSize;
-		}
-		int getHorizontalSize()
-		{
-			return horizontalSize;
-		}
-		int getStartX()
-		{
-			return startX;
-		}
-		int getStartY()
-		{
-			return startY;
-		}
-		int getEndX()
-		{
-			return endX;
-		}
-		int getEndY()
-		{
-			return endY;
-		}
-		char** getLabirynth()
-		{
-			return labirynth;
-		}
-		int setVerticalSize(int verticalSize)
-		{
-			this->verticalSize = verticalSize;
-		}
-		int setHorizontalSize(int horizontalSize)
-		{
-			this->horizontalSize = horizontalSize;
-		}
-		int setStartX(int startX)
-		{
-			this->startX = startX;
-		}
-		int setStartY(int startY)
-		{
-			this->startY = startY;
-		}
-		int setEndX(int endX)
-		{
-			this->endX = endX;
-		}
-		int setEndY(int endY)
-		{
-			this->endY = endY;
-		}
-		void setLabirynth(char** labirynth)
-		{
-			this->labirynth = labirynth;
-		}
-		void setWall(int x, int y)
-		{
-			labirynth[x][y] = 'x';
-		}
-		void setPath(int x, int y)
-		{
-			labirynth[x][y] = ' ';
-		}
-		void printLabirynth()
-		{
-			for (int i = 0; i < verticalSize; i++)
+			int random = rand() % 2;
+			if (random == 0)
 			{
-				for (int j = 0; j < horizontalSize; j++)
+				end[0] = rand() % (HEIGHT - 1);
+				end[1] = WIDTH - 1;
+				if (labirynth[end[1]][end[0] - 1] != 'x')
 				{
-					cout << labirynth[i][j] << " ";
+					labirynth[end[1]][end[0]] = 'E';
+					set_end = true;
 				}
-				cout << endl;
+			}
+			else
+			{
+				end[0] = HEIGHT - 1;
+				end[1] = rand() % (WIDTH - 1);
+				if (labirynth[end[1]][end[0] - 1] != 'x')
+				{
+					labirynth[end[1]][end[0]] = 'E';
+					set_end = true;
+				}
 			}
 		}
-		void makePath(int x, int y, int xz, int yz)
+	}
+
+	void makePath(int x, int y, int xz, int yz)
+	{
+		int currentPos[2] = {x, y};
+		int previousPos[2] = {xz, yz};
+		path.push_back(currentPos);
+		if (currentPos[0] == end[1] && currentPos[1] == end[0])
 		{
-			int currentPos[2] = { x, y };
-			int previousPos[2] = { xz, yz };
-			path.push_back(currentPos);
-			if (currentPos[0] == endX && currentPos[1] == endY)
+			cout << "You are already at the end of the labirynth" << endl;
+			char **drawnPath = new char *[HEIGHT];
+			for (int i = 0; i < HEIGHT; i++)
 			{
-				cout << "You are already at the end of the labirynth" << endl;
-				char** drawnPath = new char*[verticalSize];
-				for (int i = 0; i < verticalSize; i++)
+				drawnPath[i] = new char[WIDTH];
+				for (int j = 0; j < WIDTH; j++)
 				{
-					drawnPath[i] = new char[horizontalSize];
-					for (int j = 0; j < horizontalSize; j++)
-					{
-						drawnPath[i][j] = 'x';
-					}
-				}
-				for (int i = 0; i < path.size(); i++)
-				{
-					if (i == 0)
-					{
-						drawnPath[path[i][0]][path[i][1]] = 'S';
-					}
-					else if (i == path.size() - 1)
-					{
-						drawnPath[path[i][0]][path[i][1]] = 'E';
-					}
-					else
-					{
-						drawnPath[path[i][0]][path[i][1]] = ' ';
-					}
-				}
-				writeLabirynthToFile(drawnPath, horizontalSize, verticalSize);
-			}
-			if (currentPos[0] - 1 >= 0)
-			{
-				if (labirynth[currentPos[0] - 1][currentPos[1]] == ' ' || labirynth[currentPos[0] - 1][currentPos[1]] == 'E')
-				{
-					if (currentPos[0] - 1 != previousPos[0])
-					{
-						makePath(currentPos[0] - 1, currentPos[1], currentPos[0], currentPos[1]);
-					}
+					drawnPath[i][j] = 'x';
 				}
 			}
-			if (currentPos[1] - 1 >= 0)
+			for (int i = 0; i < path.size(); i++)
 			{
-				if (labirynth[currentPos[0]][currentPos[1] - 1] == ' ' || labirynth[currentPos[0]][currentPos[1] - 1] == 'E')
+				if (i == 0)
 				{
-					if (currentPos[1] - 1 != previousPos[1])
-					{
-						makePath(currentPos[0], currentPos[1] - 1, currentPos[0], currentPos[1]);
-					}
+					drawnPath[path[i][0]][path[i][1]] = 'S';
+				}
+				else if (i == path.size() - 1)
+				{
+					drawnPath[path[i][0]][path[i][1]] = 'E';
+				}
+				else
+				{
+					drawnPath[path[i][0]][path[i][1]] = ' ';
 				}
 			}
-			if (currentPos[0] + 1 < verticalSize)
-			{
-				if (labirynth[currentPos[0] + 1][currentPos[1]] == ' ' || labirynth[currentPos[0] + 1][currentPos[1]] == 'E')
-				{
-					if (currentPos[0] + 1 != previousPos[0])
-					{
-						makePath(currentPos[0] + 1, currentPos[1], currentPos[0], currentPos[1]);
-					}
-				}
-			}
-			if (currentPos[1] + 1 < horizontalSize)
-			{
-				if (labirynth[currentPos[0]][currentPos[1] + 1] == ' ' || labirynth[currentPos[0]][currentPos[1] + 1] == 'E')
-				{
-					if (currentPos[1] + 1 != previousPos[1])
-					{
-						makePath(currentPos[0], currentPos[1] + 1, currentPos[0], currentPos[1]);
-					}
-				}
-			}
-			path.pop_back();
+			writeLabirynthToFile(drawnPath, WIDTH, HEIGHT);
 		}
+		if (currentPos[0] - 1 >= 0)
+		{
+			if (labirynth[currentPos[0] - 1][currentPos[1]] == ' ' || labirynth[currentPos[0] - 1][currentPos[1]] == 'E')
+			{
+				if (currentPos[0] - 1 != previousPos[0])
+				{
+					makePath(currentPos[0] - 1, currentPos[1], currentPos[0], currentPos[1]);
+				}
+			}
+		}
+		if (currentPos[1] - 1 >= 0)
+		{
+			if (labirynth[currentPos[0]][currentPos[1] - 1] == ' ' || labirynth[currentPos[0]][currentPos[1] - 1] == 'E')
+			{
+				if (currentPos[1] - 1 != previousPos[1])
+				{
+					makePath(currentPos[0], currentPos[1] - 1, currentPos[0], currentPos[1]);
+				}
+			}
+		}
+		if (currentPos[0] + 1 < HEIGHT)
+		{
+			if (labirynth[currentPos[0] + 1][currentPos[1]] == ' ' || labirynth[currentPos[0] + 1][currentPos[1]] == 'E')
+			{
+				if (currentPos[0] + 1 != previousPos[0])
+				{
+					makePath(currentPos[0] + 1, currentPos[1], currentPos[0], currentPos[1]);
+				}
+			}
+		}
+		if (currentPos[1] + 1 < WIDTH)
+		{
+			if (labirynth[currentPos[0]][currentPos[1] + 1] == ' ' || labirynth[currentPos[0]][currentPos[1] + 1] == 'E')
+			{
+				if (currentPos[1] + 1 != previousPos[1])
+				{
+					makePath(currentPos[0], currentPos[1] + 1, currentPos[0], currentPos[1]);
+				}
+			}
+		}
+		path.pop_back();
+	}
+
+public:
+	void printLabirynth()
+	{
+		for (int y = 0; y < HEIGHT; y++)
+		{
+			for (int x = 0; x < WIDTH; x++)
+			{
+				cout << labirynth[y][x];
+			}
+			cout << endl;
+		}
+	}
+
+	Labirynth(int WIDTH, int HEIGHT) : WIDTH(WIDTH), HEIGHT(HEIGHT)
+	{
+		for (int i = 0; i < HEIGHT; i++)
+		{
+			vector<char> row(WIDTH, 'x');
+			labirynth.push_back(row);
+		}
+		labirynth[1][1] = ' ';
+		generateMaze(1, 1);
+		randomStartEndPoints();
+		makePath(start[1], start[0], start[1], start[0]);
+		for (int i = 0; i < path.size(); i++)
+		{
+			cout << path[i][0] << " " << path[i][1] << endl;
+		}
+	}
 };
 
 int main()
 {
-	Labirynth lab = Labirynth(10, 10, 0, 1, 9, 8);
-	lab.printLabirynth();
-	//lab.makePath(lab.getStartX(), lab.getStartY(), lab.getStartX(), lab.getStartY());
+	srand(time(NULL));
+	Labirynth labirynth(21, 21);
+	labirynth.printLabirynth();
 	return 0;
 }
